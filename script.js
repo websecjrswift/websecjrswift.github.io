a1740b883a78ed8f0aac525b4e08ffb62d0beb85
Jack-Swift714 <89950494+Jack-Swift714@users.noreply.github.com>
2021-09-13 19:14:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,117 +1,52 @@
-var smoke = document.getElementById('smoke-1');
-var smoke2 = document.getElementById('smoke-2');
-var smoke3 = document.getElementById('smoke-3');
-var smoke4 = document.getElementById('smoke-4');
-var smoke5 = document.getElementById('smoke-5');
-var smoke6 = document.getElementById('smoke-6');
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+import * as rtdb from "https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js";
+  // TODO: Add SDKs for Firebase products that you want to use
 
-var timeline = new TimelineMax({ repeat: -1 });
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-var timeline2 = new TimelineMax({ repeat: -1 });
 
-timeline.fromTo(
-  smoke,
-  2.3,
-  {
-    scale: 1,
-    x: '-10px',
-    y: '-10px',
-    opacity: 1,
-    ease: Power1.easeInOut
-  },
-  {
-    scale: 2.2,
-    opacity: 0,
-    y: '-80px',
-    ease: Power1.easeInOut
+  // Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDKrXtP70xM20FlTuHt0RuQrT2vVUStVGk",
+
+    authDomain: "practice-fc54e.firebaseapp.com",
+
+    databaseURL: "https://practice-fc54e-default-rtdb.firebaseio.com",
+
+    projectId: "practice-fc54e",
+
+    storageBucket: "practice-fc54e.appspot.com",
+
+    messagingSenderId: "433900705046",
+
+    appId: "1:433900705046:web:28781fa4b152f6f07aac37"
+
+};
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+let db = rtdb.getDatabase(app);
+
+let titleRef = rtdb.ref(db, "/");
+let chats = rtdb.child(titleRef, "chats")
+
+rtdb.onValue(chats, ss=>{
+  //alert(JSON.stringify(ss.val()));
+  let msgarray = (ss.val());
+  let newarr = Object.values(msgarray);
+  console.log(Object.values(msgarray));
+  let msgList = "";
+  for (var i = 0, msgg; msgg = newarr[i]; i++) {
+    msgList += "<li>" + msgg + "</li>";
   }
-).fromTo(
-  smoke2,
-  2.3,
-  {
-    scale: 1,
-    x: '-5px',
-    y: '-10px',
-    opacity: 1,
-    ease: Power1.easeInOut
-  },
-  {
-    scale: 2.3,
-    opacity: 0,
-    y: '-65px',
-    ease: Power1.easeInOut
-  },
-  '-=2'
-).fromTo(
-  smoke3,
-  2.3,
-  {
-    scale: 1,
-    x: '-5px',
-    y: '30px',
-    opacity: 1,
-    ease: Power1.easeInOut
-  },
-  {
-    scale: 2,
-    opacity: 0,
-    y: '-20px',
-    ease: Power1.easeInOut
-  },
-  '-=2.2'
-)
+  document.querySelector("#chatsloc").innerHTML = msgList;
+})
 
-timeline2.fromTo(
-  smoke4,
-  2.3,
-  {
-    scale: 1,
-    x: '-5px',
-    y: '-10px',
-    opacity: 1,
-    ease: Power1.easeInOut
-  },
-  {
-    scale: 2.6,
-    opacity: 0,
-    y: '-80px',
-    ease: Power1.easeInOut
-  }
-).fromTo(
-  smoke5,
-  2.3,
-  {
-    scale: 1,
-    x: '-10px',
-    y: '-10px',
-    opacity: 1,
-    ease: Power1.easeInOut
-  },
-  {
-    scale: 2.8,
-    opacity: 0,
-    y: '-70px',
-    ease: Power1.easeInOut
-  },
-  '-=1.8'
-).fromTo(
-  smoke6,
-  2.3,
-  {
-    scale: 1,
-    x: '0px',
-    y: '5px',
-    opacity: 1,
-    ease: Power1.easeInOut
-  },
-  {
-    scale: 2.4,
-    opacity: 0,
-    y: '-70px',
-    ease: Power1.easeInOut
-  },
-  '-=2.0'
-)
+var clickHandlerMSG = function(){
+  var msg = document.querySelector("#msg").value;
+  rtdb.push(chats, msg);
+}
 
-timeline.timeScale(0.8);
-timeline2.timeScale(0.7);
+document.querySelector("#sendmsg").addEventListener("click", clickHandlerMSG);
