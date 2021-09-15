61b1db7c87e8a906a84a57302b281dffaab33ae9
Jack-Swift714 <89950494+Jack-Swift714@users.noreply.github.com>
2021-09-15 10:40:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,58 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
 import * as rtdb from "https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js";
   // TODO: Add SDKs for Firebase products that you want to use
-
   // https://firebase.google.com/docs/web/setup#available-libraries
-
-
   // Your web app's Firebase configuration
 
 const firebaseConfig = {
-
     apiKey: "AIzaSyDKrXtP70xM20FlTuHt0RuQrT2vVUStVGk",
-
     authDomain: "practice-fc54e.firebaseapp.com",
-
     databaseURL: "https://practice-fc54e-default-rtdb.firebaseio.com",
-
     projectId: "practice-fc54e",
-
     storageBucket: "practice-fc54e.appspot.com",
-
     messagingSenderId: "433900705046",
-
     appId: "1:433900705046:web:28781fa4b152f6f07aac37"
-
 };
-
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 let db = rtdb.getDatabase(app);
-
 let titleRef = rtdb.ref(db, "/");
 let chats = rtdb.child(titleRef, "chats")
+$("#chatsloc").html("no messages");
+
+
+var clickHandlerUser = function(){
+  var username = $("#usertext").val();
+  $("#usernamedisplay").html("hello " + username);
+}
+
+var clickHandlerMSG = function(){
+  console.log(username);
+  var msg = $("#msg").val();
+  var username = $("#usertext").val();
+  var chatmsg = {message: msg, User: username};
+  //console.log(chatmsg);
+  rtdb.push(chats, chatmsg);
+}
 
 rtdb.onValue(chats, ss=>{
   //alert(JSON.stringify(ss.val()));
-  let msgarray = (ss.val());
-  let newarr = Object.values(msgarray);
-  console.log(Object.values(msgarray));
-  let msgList = "";
-  for (var i = 0, msgg; msgg = newarr[i]; i++) {
-    msgList += "<li>" + msgg + "</li>";
-  }
-  document.querySelector("#chatsloc").innerHTML = msgList;
+  $("#chatsloc").empty();
+  ss.forEach(function(el){
+    let msg = JSON.stringify(el.val().message);
+    let user = JSON.stringify(el.val().User);
+    let userinput = user.replace(/"/g, '');
+    $("#chatsloc").append("<li>" + userinput + ":" + msg + "</li>");
+  })
 })
 
-var clickHandlerMSG = function(){
-  var msg = document.querySelector("#msg").value;
-  rtdb.push(chats, msg);
-}
 
 var clickHandlerClear = function(){
-  var msg = document.querySelector("#msg").value;
+  var msg = $("#msg").val();
   rtdb.set(chats, []);
-  document.querySelector("#chatsloc").innerHTML = "no messages";
+  $("#chatsloc").html("no messages");
 }
 
-document.querySelector("#sendmsg").addEventListener("click", clickHandlerMSG);
-document.querySelector("#clear").addEventListener("click", clickHandlerClear);
+$("#sendmsg").click(clickHandlerMSG);
+$("#clear").click(clickHandlerClear);
+$("#inputuser").click(clickHandlerUser);
