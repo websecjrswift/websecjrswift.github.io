a515ff7757ec2ad2fa2f8621834185c067542c1e
Jack-Swift714 <89950494+Jack-Swift714@users.noreply.github.com>
2021-09-20 19:45:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,34 +18,83 @@ const app = initializeApp(firebaseConfig);
 let db = rtdb.getDatabase(app);
 let titleRef = rtdb.ref(db, "/");
 let chats = rtdb.child(titleRef, "chats")
-$("#chatsloc").html("no messages");
+let users = rtdb.child(titleRef, "users")
 
+$("#chatsloc").html("no messages");
 
 var clickHandlerUser = function(){
   var username = $("#usertext").val();
-  $("#usernamedisplay").html("hello " + username);
+  let usertext = $("#usernamedisplay").text();
+  if (usertext == '') {
+    rtdb.push(users, username);
+  }
+  else 
+  {
+    alert("you are already signed in");
+  }
+  //$("#usernamedisplay").html(username);
 }
 
 var clickHandlerMSG = function(){
   console.log(username);
   var msg = $("#msg").val();
-  var username = $("#usertext").val();
+  var username = $("#usernamedisplay").text();
   var chatmsg = {message: msg, User: username};
-  //console.log(chatmsg);
   rtdb.push(chats, chatmsg);
+}
+
+var clickHandlerEdit = function(target){
+  let curTarget = target.currentTarget;
+  var curID = $(curTarget).attr("data-id");
+  let msg = this.innerHTML;
+  let ind = msg.indexOf(":");
+  let msgUser = msg.slice(0, ind);
+  var username = $("#usernamedisplay").text();
+  if (username == msgUser){
+    let edit = window.prompt("Edit your Message","");
+    this.innerHTML = username + ':"' + edit + '"';
+    let message = rtdb.ref(db, "chats/" + curID + "/message");
+    rtdb.set(message, edit)
+  }
+  else {
+    alert("you can't edit this message");
+  }
 }
 
 rtdb.onValue(chats, ss=>{
   //alert(JSON.stringify(ss.val()));
   $("#chatsloc").empty();
-  ss.forEach(function(el){
-    let msg = JSON.stringify(el.val().message);
-    let user = JSON.stringify(el.val().User);
-    let userinput = user.replace(/"/g, '');
-    $("#chatsloc").append("<li>" + userinput + ":" + msg + "</li>");
-  })
-})
+  if (ss.val() != null){
+    let msgIDs = Object.keys(ss.val());
+    msgIDs.map((anId)=>{
+      let msg = JSON.stringify(ss.val()[anId].message);
+      let user = JSON.stringify(ss.val()[anId].User);
+      let userinput = user.replace(/"/g, '');
+      $("#chatsloc").append(
+        `<div class="msg" data-id=${anId}>${userinput + ":" + msg}</div>`
+      );
+    });
+    $(".msg").click(clickHandlerEdit);
+  };
+});
 
+rtdb.onValue(users, ss=>{
+  $("#userLoc").empty();
+  let usertext = $("#usernamedisplay").text();
+  if (ss.val() != null){
+    let userIDs = Object.keys(ss.val());
+    userIDs.map((anId)=>{
+      let user = JSON.stringify(ss.val()[anId]);
+      let userinput = user.replace(/"/g, '');
+      $("#userLoc").append(
+        `<div class="user" data-id=${anId}>${userinput}</div>`
+      );  
+      $("#usernamedisplay").html(
+        `<div class="user" data-id=${anId}>${userinput}</div>`
+      );
+    });
+  }
+});
 
 var clickHandlerClear = function(){
   var msg = $("#msg").val();
@@ -53,6 +102,20 @@ var clickHandlerClear = function(){
   $("#chatsloc").html("no messages");
 }
 
+var clickHandlerSO = function(){
+  let username = $("#usernamedisplay").text();
+  if (username != ''){
+    let htmltag = JSON.stringify($("#usernamedisplay").html());
+    let ind = htmltag.indexOf("data-id");
+    let ind2 = htmltag.indexOf(">");
+    let userID = htmltag.slice(ind + 10, ind2 - 2);
+    let userRef = rtdb.ref(db, "users/" + userID)
+    rtdb.set(userRef, null);
+    $("#usernamedisplay").html('');
+  }
+}
+
 $("#sendmsg").click(clickHandlerMSG);
 $("#clear").click(clickHandlerClear);
 $("#inputuser").click(clickHandlerUser);
+$("#signout").click(clickHandlerSO);
